a125ddcc9fe4bab122f6372551df194b9b674dd9
vgpro54321 <vladimir.gaytanov.pro@gmail.com>
2022-01-16 08:51:45 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import {
   Subscriber,
   throwError,
   concatWith,
+  OperatorFunction,
 } from 'rxjs';
 import { take } from 'rxjs/operators';
 
@@ -49,6 +50,19 @@ function filterNil() {
     });
   };
 }
+function filterNilV2() {
+  return function <T>(source: Observable<T>): Observable<T> {
+    return new Observable((subscriber) => {
+      return source.subscribe({
+        next(value) {
+          if (value !== undefined && value !== null) {
+            subscriber.next(value);
+          }
+        },
+      });
+    });
+  };
+}
 
 function subscriberCount<T>(
   sourceObservable: Observable<T>,
@@ -74,10 +88,63 @@ function subscriberCountOperator<T>() {
   };
 }
 
+type ReferenceCountingFunc = () => number;
+
+function subscriberCounter<T>(
+  sourceObservable: Observable<T>,
+  addRef: ReferenceCountingFunc,
+  removeRef: ReferenceCountingFunc
+): Observable<T> {
+  return new Observable((subscriber: Subscriber<T>) => {
+    const subscription = sourceObservable.subscribe(subscriber);
+    addRef();
+
+    return () => {
+      subscription.unsubscribe();
+      removeRef();
+    };
+  });
+}
+
+class subscruberCounterFactory {
+  private refCount: number = 0;
+
+  addRef(): number {
+    this.refCount++;
+    console.log('addRef', this.refCount);
+    return this.refCount;
+  }
+
+  removeRef(): number {
+    this.refCount--;
+    console.log('removRef', this.refCount);
+    return this.refCount;
+  }
+
+  getOperator<T>(): OperatorFunction<T, T> {
+    let self = this;
+
+    return function <T>(source: Observable<T>): Observable<T> {
+      return subscriberCounter<T>(
+        source,
+        () => {
+          return self.addRef();
+        },
+        () => {
+          return self.removeRef();
+        }
+      );
+    };
+  }
+}
+
+let factory = new subscruberCounterFactory();
 //let op$ = subscriberCount(o$, 'Experiment');
 //let op$ = o$.pipe();
 //let op$ = throwError(new Error('From start'));
-let op$ = o$.pipe(subscriberCountOperator());
+//let op$ = o$.pipe(subscriberCountOperator());
+
+let op$ = o$.pipe(factory.getOperator());
 let sub = op$.subscribe((x) => {
   console.log('operator = ', x);
 
