3375eff496ee2d65569916a68ec23c6b49d0d691
vgpro54321 <vladimir.gaytanov.pro@gmail.com>
2022-01-16 07:17:13 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 // Import stylesheets
 import './style.css';
+import { of } from 'rxjs';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+console.log('app start');
+
+let o$ = of(1, 2, 3);
+o$.subscribe((x) => {
+  console.log(x);
+});
